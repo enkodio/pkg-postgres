6d94b621880c818f77b1e3d2b60b370357c7ec9e
konstantin.litvin <example@gmail.com>
2023-10-08 17:22:22 +0300

    fix logger

--- client/postgres.go
+++ client/postgres.go
@@ -2,11 +2,17 @@ package client
 
 import (
 	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	cfgEntity "gitlab.enkod.tech/pkg/postgres/pkg/config/entity"
+	"gitlab.enkod.tech/pkg/postgres/pkg/logger"
 )
 
-func NewClient(cfg cfgEntity.Config, serviceName string) (RepositoryClient, Transactor) {
+func NewClient(cfg cfgEntity.Config, serviceName string, log *logrus.Logger) (RepositoryClient, Transactor) {
+	if log != nil {
+		logger.SetLogger(log)
+	} else {
+		logger.SetDefaultLogger("debug")
+	}
 	client, tx, err := newClient(cfg, serviceName)
 	if err != nil {
 		log.Fatal(errors.Wrap(err, "cant create pg client"))
